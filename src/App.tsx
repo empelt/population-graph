846422a4f6ceff34311c79f6542cbe60acd5068f
empelt <tetsuyakato2001@yahoo.co.jp>
2022-12-24 21:13:35 +0900

    チェックボックスコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <Section>
+        <SectionTitle>グラフに表示する都道府県を選択してください</SectionTitle>
+      </Section>
+      <Section>
+        <p>グラフ</p>
+      </Section>
+    </PageContainer>
   )
 }
+
+const PageContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`
+
+const Section = styled.section`
+  padding: 10px 5px 10px 5px;
+`
+
+const SectionTitle = styled.p`
+  text-align: center;
+  margin-bottom: 10px;
+`
---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type Props = {
+  checked?: boolean
+  onChange: (prefCode: number, isChecked: boolean) => void
+  prefCode: number
+  prefName: string
+}
+
+export default function CheckBox({
+  checked = false,
+  onChange,
+  prefCode,
+  prefName,
+}: Props) {
+  return (
+    <Label>
+      <Input
+        type="checkbox"
+        checked={checked}
+        onChange={(event) => onChange(prefCode, event.target.checked)}
+      />
+      <Span checked={checked}>{prefName}</Span>
+    </Label>
+  )
+}
+
+const Label = styled.label`
+  display: contents;
+`
+
+const Span = styled.span<{ checked: boolean }>`
+  color: ${({ checked }) => (checked ? 'white' : '#1a202c')};
+  font-size: 14px;
+  background-color: ${({ checked }) => (checked ? '#1a202c' : 'white')};
+  box-shadow: 0px 0px 15px -7px #777777;
+  border-radius: 20px;
+  padding: 5px 20px;
+  transition: box-shadow 0.2s ease-out 0s;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 0px 15px -3px #777777;
+  }
+`
+
+const Input = styled.input`
+  display: none;
+`
